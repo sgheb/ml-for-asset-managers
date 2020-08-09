12f5d212062cc5469d778a82eb5bc5a400bc3614
Sacha G <sacha.gheb@gmail.com>
2020-08-09 19:24:40 -0400

    issue #1: added snippets

--- code_snippets/chap2.py
+++ code_snippets/chap2.py
@@ -16,7 +16,7 @@ def mpPDF(var, q, pts):
 def getPCA(matrix):
     """Get eVal, eVec from a Hermitian matrix"""
     eVal, eVec = np.linalg.eigh(matrix)
-    indices = eVal.argsort()[::-1]  # artuments for sortinig eVal desc
+    indices = eVal.argsort()[::-1]  # arguments for sorting eVal desc
     eVal, eVec = eVal[indices], eVec[:, indices]
     eVal = np.diagflat(eVal)
     return eVal, eVec
